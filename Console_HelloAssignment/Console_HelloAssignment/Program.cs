3de819c712924746c0fd727ad3c56b83a977d16e
Brie Ellis <38733065+briekellis@users.noreply.github.com>
2019-01-16 15:25:32 -0500

    Add files via upload

---
+++ Console_HelloAssignment/Console_HelloAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_HelloAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteDonut;
+
+            Console.WriteLine("What is your favorite donut?");
+            favoriteDonut = Console.ReadLine();
+            Console.WriteLine("Your favorite donut is... " + favoriteDonut);
+            Console.Read();
+        }
+    }
+}
